17eef67cb9622f0d22d0176f21ba1971f2b74917
StephenMullori <70962083+StephenMullori@users.noreply.github.com>
2020-10-24 19:05:06 -0400

    Update Fractions.java

--- Fractions.java
+++ Fractions.java
@@ -4,59 +4,91 @@
  *Description: Unit 1 Final Project
 */
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Fractions {
-	static int num1;
-	static int den1;
-	static int num2;
-	static int den2;
-	Scanner scanner = new Scanner(System.in);
+	static int num;
+	static int den;
 	
-	public static void Fraction() { 
-		num1 = 1;
-		den1 = 2;	
-		
-		num2 = 1;
-		den2 = 2;	
+	static Random rand = new Random();
+	
+	public Fractions() { 
+		Fractions.num = 1; 
+		Fractions.den = 2;	
+	
 	}
 	
-	public static void adding(int num1, int den1, int num2, int den2) {
+	public Fractions(int num, int den) {
+		Fractions.num = num;
+		Fractions.den = den;
+	}
+	
+	public static void adding(int num,int den) {
+		update();
+		int num1 = Fractions.num;
+		int den1 = Fractions.den; 
+		update1();
+		int num2 = Fractions.num;
+		int den2 = Fractions.den;
+		
+		int endnum = (num1 * den2) + (num2 * den1);
 		int endden = den1 * den2;
-		int endnum = num1 + num2;
 		System.out.println(num1  + "/" + den1 + " + " + num2 + "/" + den2 + " = " + endnum + "/" + endden);
 	}
 	
-	public static void sub(int num1, int den1, int num2, int den2) {
+	public static void sub(int num, int den) {
+		update();
+		int num1 = Fractions.num;
+		int den1 = Fractions.den; 
+		update1();
+		int num2 = Fractions.num;
+		int den2 = Fractions.den;
 		int endden = den1 * den2;
-		int endnum = num1 - num2;
+		int endnum = (num1 * den2) - (num2 * den1);
 		System.out.println(num1  + "/" + den1 + " - " + num2 + "/" + den2 + " = " + endnum + "/" + endden);
 	}
 
-	public static void multi(int num1, int den1, int num2, int den2) {
+	public static void multi(int num, int den) {
+		update();
+		int num1 = Fractions.num;
+		int den1 = Fractions.den; 
+		update1();
+		int num2 = Fractions.num;
+		int den2 = Fractions.den;
 		int endden = den1 * den2;
 		int endnum = num1 * num2;
 		System.out.println(num1  + "/" + den1 + " x " + num2 + "/" + den2 + " = " + endnum + "/" + endden);
 	}
 
-	public static void divde(int num1, int den1, int num2, int den2) {
+	public static void divde(int num, int den) {
+		update();
+		int num1 = Fractions.num;
+		int den1 = Fractions.den; 
+		update1();
+		int num2 = Fractions.num;
+		int den2 = Fractions.den;
 		int endden = den1 * num2;
 		int endnum = num1 * den2;
 		System.out.println(num1  + "/" + den1 + " / " + num2 + "/" + den2 + " = " + endnum + "/" + endden);
 	}
+
+	public static void update() {
+		Fractions fraction = new Fractions();
+	}
+	
+	public static void update1() {
+		Fractions fraction2 = new Fractions(5, 12);
+	}
 	
 	public static void main(String[] args) {
-	
-	Fraction();
-	
-	adding(num1, den1, num2, den2);
-	
-	sub(num1, den1, num2, den2);
-	
-	multi(num1, den1, num2, den2);
-	
-	divde(num1, den1, num2, den2);
-	
-	}
+		adding(Fractions.num, Fractions.den);
+		
+		sub(Fractions.num, Fractions.den);
 
+		multi(Fractions.num, Fractions.den);
+	
+		divde(Fractions.num, Fractions.den);
+	}
+		
+		
 }
